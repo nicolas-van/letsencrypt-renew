2e4e3b74cb1216a8f69643f4bd7727569160d86a
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2016-05-13 11:46:25 +0200

    Update renew-ssl-certs.bash

--- renew-ssl-certs.bash
+++ renew-ssl-certs.bash
@@ -3,6 +3,6 @@
 service nginx stop
 echo >> /var/log/renew-ssl-certs.log
 date >> /var/log/renew-ssl-certs.log
-echo ============================= >> /var/log/renew-ssl-certs.lo
+echo ============================= >> /var/log/renew-ssl-certs.log
 /root/letsencrypt/letsencrypt-auto renew >> /var/log/renew-ssl-certs.log 2>&1
 service nginx start
